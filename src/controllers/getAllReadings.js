d2a9d0472fd065514a98692bd46d54abcbcd8d64
Nicolas Kwiatkowski <nicolasm.kw@gmail.com>
2024-03-29 19:36:50 -0300

    Deleted a console.log() in getAllReadings.js

--- src/controllers/getAllReadings.js
+++ src/controllers/getAllReadings.js
@@ -3,7 +3,6 @@ const { Monitoring } = require('../config/db');
 module.exports = async function getAllReadings(req, res) {
     try {
         const metrics = await Monitoring.findAll();
-        console.log(metrics)
         res.status(200).json(metrics);
 
     } catch (error) {
